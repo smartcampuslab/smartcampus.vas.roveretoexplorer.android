e1692449fde2a59d6fe46ad5c2fac46b0713abfc
chin8 <m.chini@trentorise.eu>
2014-02-13 14:41:18 +0100

    New app liked to the new common library

--- RoveretoExplorer/src/eu/iescities/pilot/rovereto/roveretoexplorer/RoveretoExplorerApplication.java
+++ RoveretoExplorer/src/eu/iescities/pilot/rovereto/roveretoexplorer/RoveretoExplorerApplication.java
@@ -20,10 +20,12 @@ import android.app.Application;
 import android.content.Context;
 import android.os.Build;
 import android.os.StrictMode;
+
 import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
+
 
 
 public class RoveretoExplorerApplication extends Application {
